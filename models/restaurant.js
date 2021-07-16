06b4e465606fe44cde51c4e8ba0116cc559e27a9
pinjin1201 <knowledge84121@gmail.com>
2021-07-16 17:27:34 +0800

    add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,7 +5,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
+  id: {
+    type: String,
+    required: true
+  },
   name: {
+    type: String,
+    required: true
+  },
+  name_en: {
     type: String,
     required: true
   },
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,38 @@
+// 載入 mongoose
+const mongoose = require('mongoose')
+
+// 載入 restaurant model
+const Restaurant = require('../restaurant')
+
+// 載入 restaurant data
+const restaurantList = require('../../restaurant.json')
+const seed = restaurantList.results
+
+// 設定連線到 mongodb
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  seed.forEach(restaurant => {
+    Restaurant.create({
+      id: restaurant.id,
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description
+    })
+  })
+})
