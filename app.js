eb49ea13d9e0062852cabf8a9ed31ad9a6f369c4
pinjin1201 <knowledge84121@gmail.com>
2021-07-17 11:33:43 +0800

    add create restaurant function

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ const exphbs = require('express-handlebars')
 // 載入 restaurant model
 const Restaurant = require('./models/restaurant')
 
+// 載入 body-parser
+const bodyParser = require('body-parser')
+
 // 設定 把樣板引擎交給 Express-handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // 設定靜態檔案位置
 app.use(express.static('public'))
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 載入 mongoose
 const mongoose = require('mongoose')
@@ -35,7 +41,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定首頁路由
+// 首頁路由
 app.get('/', (req, res) => {
   // 取出 Todo model 裡的所有資料
   Restaurant.find()
@@ -45,6 +51,31 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants  }))
     // 錯誤處理
     .catch(error => console.error(error)) 
+})
+
+// 設定新增資料路由
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// 新建的資料傳入資料庫
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en  = req.body.name_en 
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  const restaurant = new Restaurant({ name, name_en, category, image, location, phone, google_map, rating, description
+  })
+
+  return restaurant.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // 監聽本機伺服器 port 3000
